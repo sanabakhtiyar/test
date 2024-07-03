6f554a5f41925c9a73cfb214fce33843ac030fc9
Sana op <hinakanwalhaq@gmail.com>
2024-07-03 23:00:33 +0500

    Done

---
+++ index.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+    <h1>Sana Waqas </h1>
+    <h1>Class:5</h1>
+    <h1>10</h1>
+    <h1>Roll Number:4990</h1>
+</head>
+<body>
+<a href="C:/Users/Sana%20Waqas/Desktop/Web%20devlement%20crash/HTML/1index.html">Task 1</a>
+<a href="C:/Users/Sana%20Waqas/Desktop/Web%20devlement%20crash/HTML/%2C%20%20index.html">Task 2</a>
+<a href="C:/Users/Sana%20Waqas/Desktop/Web%20devlement%20crash/HTML/1index.html">Task 3</a>
+<a href="C:/Users/Sana%20Waqas/Desktop/Web%20devlement%20crash/HTML/index.html">Task 4</a>
+
+
+
+
+</body>
+</html>
